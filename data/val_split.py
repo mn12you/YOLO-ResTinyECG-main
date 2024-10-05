af9db652d8102aa6365e2bddab2a5bf18f07725b
mn12you <76604100+mn12you@users.noreply.github.com>
2024-10-05 23:40:22 +0800

    modified:   .gitignore 	new file:   data/val_split.py

---
+++ data/val_split.py
@@ -0,0 +1,20 @@
+import random
+random.seed(42)
+ratio=0.2
+# Read the contents of the file
+with open('data/AHA_10s_rev/val.txt', 'r') as file:
+    lines = file.readlines()
+
+# Shuffle the rows
+random.shuffle(lines)
+
+# Calculate 20% and 80% split
+split_index = int(len(lines) *ratio)
+
+# Write 20% of the shuffled rows to a new file
+with open('data/AHA_10s_rev/train.txt', 'w') as file:
+    file.writelines(lines[:split_index])
+
+# Write 80% of the shuffled rows back to the original file (or to another file if preferred)
+with open('data/AHA_10s_rev/val.txt', 'w') as file:
+    file.writelines(lines[split_index:])
